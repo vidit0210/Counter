dd92352792347be7090e06c9549d5e8f71f6a131
Vidit Shah <vidit0210@gmail.com>
2018-12-11 08:24:21 +0530

    ClassName Attr

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
+
 class Counter extends Component {
   state = {
     count: 0
+    // imageUrl: "https://picsum.photos/200"
+  };
+
+  styles = {
+    fontSize: 40,
+    fontWeight: "italic"
+    //Implementation-><span style={this.styles} className="badge badge-primary m-2">
   };
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={{ fontSize: 10 }} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? <h2>Zero</h2> : count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
